dd3a63f67ab7821ccdbe36963d796d921fde2905
Yejin-Han <yain3333@gmail.com>
2022-12-27 12:56:32 +0900

    221227 modification1

--- script/top.js
+++ script/top.js
@@ -2,5 +2,13 @@ const pageLocation=document.querySelector('.page_location');
 const pageLocation_li=pageLocation.querySelectorAll('li');
 const pageLocationSub=pageLocation.querySelectorAll('.sub');
 pageLocationSub.forEach(elem=>{
-  elem.style.bottom=`${pageLocation_li[0].clientHeight}px`;
+  elem.style.bottom=`${pageLocation_li[0].clientHeight-2}px`;
+});
+pageLocation_li.forEach(elem=>{
+  elem.addEventListener('mouseenter',function(){
+    this.querySelector('a').classList.add('active');
+  });
+  elem.addEventListener('mouseleave',function(){
+    this.querySelector('a').classList.remove('active');
+  });
 });
