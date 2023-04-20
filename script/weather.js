653768bb23001f3706cbb3453ddd7048ba4902ee
Yejin-Han <yain3333@gmail.com>
2023-04-20 22:28:24 +0900

    230420 update
    리드미용 캡쳐 추가

--- script/weather.js
+++ script/weather.js
@@ -38,59 +38,59 @@ WEATHER_URL += "&base_date=" + today;
 WEATHER_URL += "&base_time=" + hours +"00";
 WEATHER_URL += "&nx=" + _nx + "&ny=" + _ny;
 
-const weatherFetch = (deg, descr, icon) => {
-  fetch(WEATHER_URL)
-	.then(res => res.json())
-	.then(data => {
-		const rData = data.response.body.items.item;
-		console.log(rData);
-		let tempArr = [];
-		let rainArr = [];
-		let skyArr = [];
-		/*rData을 순회하는 코드로 작성해야 category에 접근할 수 있을 것 같음 */
-		rData.forEach(elem=>{
-			if(elem.category == 'T1H'){
-				tempArr.push(elem.fcstValue);
-				deg.innerText = `${tempArr[0]}°C`;
-			} else if(elem.category == 'PTY'){
-				rainArr.push(elem.fcstValue);
-			} else if(elem.category == 'SKY'){
-				skyArr.push(elem.fcstValue);
-			}
-		});
-		console.log(rainArr);
-		console.log(skyArr);
-		if(rainArr[0] == '0'){
-			switch(skyArr[0]){
-				case '1':
-					descr.innerText="맑음";
-					icon.classList.add('fa-solid', 'fa-sun');
-					break;
-				case '3':
-				case '4':
-					descr.innerText="흐림";
-					icon.classList.add('fa-solid', 'fa-cloud');
-					break;
-			}
-		} else{
-			switch(rainArr[0]){
-				case '1':
-				case '5':
-					descr.innerText="비";
-					icon.classList.add('fa-solid', 'fa-umbrella');
-					break;
-				case '2':
-				case '6':
-					descr.innerText="눈/비";
-					icon.classList.add('fa-solid', 'fa-cloud-rain');
-					break;
-				case '3':
-				case '7':
-					descr.innerText="눈";
-					icon.classList.add('fa-regular', 'fa-snowflake');
-					break;
-			}
-		}
-	})
-	.catch(err => console.log(err));
+const weatherFetch = async(deg, descr, icon) => {
+  try {
+    const res = await fetch(WEATHER_URL);
+    const data = await res.json();
+    const rData = data.response.body.items.item;
+    console.log(rData);
+    let tempArr = [];
+    let rainArr = [];
+    let skyArr = [];
+    rData.forEach(elem=>{
+      if(elem.category == 'T1H'){
+        tempArr.push(elem.fcstValue);
+        deg.innerText = `${tempArr[0]}°C`;
+      } else if(elem.category == 'PTY'){
+        rainArr.push(elem.fcstValue);
+      } else if(elem.category == 'SKY'){
+        skyArr.push(elem.fcstValue);
+      }
+    });
+    console.log(rainArr);
+    console.log(skyArr);
+    if(rainArr[0] == '0'){
+      switch(skyArr[0]){
+        case '1':
+          descr.innerText="맑음";
+          icon.classList.add('fa-solid', 'fa-sun');
+          break;
+        case '3':
+        case '4':
+          descr.innerText="흐림";
+          icon.classList.add('fa-solid', 'fa-cloud');
+          break;
+      }
+    } else{
+      switch(rainArr[0]){
+        case '1':
+        case '5':
+          descr.innerText="비";
+          icon.classList.add('fa-solid', 'fa-umbrella');
+          break;
+        case '2':
+        case '6':
+          descr.innerText="눈/비";
+          icon.classList.add('fa-solid', 'fa-cloud-rain');
+          break;
+        case '3':
+        case '7':
+          descr.innerText="눈";
+          icon.classList.add('fa-regular', 'fa-snowflake');
+          break;
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
 }
